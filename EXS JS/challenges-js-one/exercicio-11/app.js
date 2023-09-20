80e0a1042fef96eefb4e05f2ffa1beea5913df22
Thiago Tavares <137300649+ThiagoTavaresDev@users.noreply.github.com>
2023-09-20 16:03:31 -0300

    exercícios resolvidos de javascript

---
+++ EXS JS/challenges-js-one/exercicio-11/app.js
@@ -0,0 +1,105 @@
+/*
+  01
+
+  - Declare uma constante que recebe o seu nome;
+  - Ela deve ter um escopo global.
+*/
+
+var name = 'Thiago'
+
+
+/*
+  02
+
+  - Funções também são blocos de código;
+  - Crie uma função que ao ser invocada, exibe sua idade no console;
+  - Dentro da função, declare uma let que armazena a sua idade;
+  - Exiba sua idade no console, através da invocação da função;
+  - Do lado de fora da função exiba no console o valor da let que foi declarada  
+    dentro da função;
+  - Um erro com a mensagem "Uncaught ReferenceError: SUA_VARIÁVEL is not  
+    defined" será exibido no console;
+  - Você sabe por que isso aconteceu?
+*/
+
+function invokeName(){
+    let idade = 18;
+    console.log('Thiago ' + idade)
+    
+
+
+}
+invokeName()
+// por conta do escopo de função o let não foi transpassado para fora, sendo assim podemos definir outro let com o mesmo nome da variável que esta dentro da função console.log(idade)
+
+
+
+/*
+  03
+
+  - Crie um objeto "car" com as seguintes propriedades e métodos:
+    - name, que recebe o nome do carro;
+    - brand, que recebe a marca do carro;
+    - colors, que recebe 3 cores para o carro;
+    - isRunning, que recebe um boolean indicando se o carro está em movimento.  
+      Inicialmente, deve receber um boolean indicando que o carro não está em  
+      movimento;
+    - run, que é um método que faz o carro andar e retorna a mensagem  
+      "O NOME_DO_CARRO está em movimento";
+    - stop, que é um método que faz o carro parar e retorna a mensagem  
+      "O NOME_DO_CARRO está parado";
+    - getColorsMessage, que é um método que retorna a mensagem  
+      "O NOME_DO_CARRO está disponível nas cores COR_01, COR_02 e COR_03".
+*/
+
+const car = {
+    name: 'Pálio',
+    brand: 'Fiat',
+    colors: ['red','blue','green'],
+    isRunning: false,
+    run: () =>{
+        console.log(`${car.name} está em movimento`)
+    },
+    stop: () =>{
+        console.log(`${car.name} está parado`)
+    },
+    getColorsMessage: ()=>{
+        console.log(`${car.name} está disponível nas cores ${car.colors}`)
+
+    }
+}
+
+
+/*
+  04
+
+  - Faça o carro andar e exiba no console se ele realmente está em movimento.
+*/
+
+car.run()
+
+/*
+  05
+
+  - Faça o carro parar e exiba no console se ele realmente está parado.
+*/
+
+car.stop()
+
+/*
+  06
+
+  - Exiba, no console, a mensagem com as cores do carro.
+*/
+
+
+car.getColorsMessage()
+
+/*
+  07
+
+  - Exiba, no console, a mensagem "O carro é um MARCA_DO_CARRO NOME_DO_CARRO";
+  - Utilize a notação de colchetes para acessar as propriedades do carro.
+*/
+
+console.log(`O carro é uma marca ${car['brand']} nome ${car['name']}`);
---
+++ EXS JS/challenges-js-one/exercicio-14/app.js
@@ -0,0 +1,120 @@
+/*
+  01
+
+  - Faça com que o texto do h1 do index.html seja exibido com todas as letras  
+    maiúsculas.
+*/
+const mainTitle = document.querySelector('#main-title')
+
+mainTitle.textContent = mainTitle.textContent.toUpperCase()
+
+/*
+  02
+
+  - Adicione, na ul vazia do index.html, os números do array "numbers" abaixo;
+  - Cada número deve estar dentro de uma <li> com a classe "number".
+*/
+
+const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
+
+const ul = document.querySelector('.numbers')
+
+function createAndInsert(){
+  
+    
+   // for(let i = 0; i < numbers.length; i++){
+    //    const newli = document.createElement('li')
+    //    newli.textContent = numbers[i]
+    //    ul.appendChild(newli)
+
+  //  }
+    numbers.forEach(number => {
+        const newli = document.createElement('li')
+        newli.textContent = number
+        ul.appendChild(newli)
+    })
+    return ul
+
+
+}
+createAndInsert()
+
+/*
+  03
+
+  - Modifique as cores dos números dentro da ul da seguinte forma:
+    - Se o número é par, ele deve ser exibido na cor "lightblue";
+    - Se o número é ímpar, exiba-o na cor "pink".
+*/
+
+function modifyColors(){
+    for(let i = 0; i < numbers.length; i++){
+        if(numbers[i] % 2 == 0){
+            const newli = document.createElement('li')
+            newli.textContent = numbers[i]
+            newli.style.color = 'lightblue'
+            ul.appendChild(newli)
+        }
+        else{
+            const newli = document.createElement('li')
+            newli.textContent = numbers[i]
+            newli.style.color = 'pink'
+            ul.appendChild(newli)
+        }
+
+    }
+
+}
+modifyColors()
+
+/*
+  04
+
+  - Adicione a classe "body-background" no corpo do index.html;
+
+  P.s: a classe "body-background" já está declarada no style.css.
+*/
+
+
+
+/*
+  05
+
+  - Adicione o link 
+    https://github.com/roger-melo-treinamentos/curso-de-js-roger-melo no href  
+    do link do index.html.
+*/
+
+
+
+/*
+  06
+
+  - Exiba o novo valor do atributo href do link no console.
+*/
+
+
+
+/*
+  07
+
+  - Exiba, no console, um objeto com todas as propriedades CSS que podem ser  
+    manipuladas via JS no h1.
+*/
+
+
+
+/*
+  08
+
+  - Remova a classe "body-background", do elemento body.
+*/
+
+
+
+/*
+  09
+
+  - Se o link da página possuir uma classe "link", remova-a;
+  - Não utilize o método remove() para fazer isso.
+*/
